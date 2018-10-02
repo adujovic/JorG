7bebf2a3172be928efe43cfff27f47efff0770a1
Mellechowicz <apkadzielawa@gmail.com>
2018-10-02 16:20:33 +0200

    Changes to be committed: 	deleted:    supercell/data.xyz 	new file:   supercell/output/POSCAR 	new file:   supercell/output/crystal.xyz 	modified:   supercell/supercell.py

--- supercell/supercell.py
+++ supercell/supercell.py
@@ -14,6 +14,7 @@ cell = []
 cellVolume = 0.0
 cellCenter = np.zeros(3)
 cellAtoms = np.array([],dtype=np.int)
+cellAtomsCopy = np.copy(cellAtoms)
 atomNames = []
 cellSize = 1
 cellInputType = 'x'
@@ -53,9 +54,11 @@ with open("POSCAR","r+") as inFile:
                 atomNames=line[:-1].split(" ")
             else:    
                 cellAtoms = np.fromstring(line,sep=" ",dtype=np.int)
+                cellAtomsCopy = np.copy(cellAtoms)
                 cellSize = np.sum(cellAtoms)
         if i == 6 and offset == 1:
             cellAtoms = np.fromstring(line,sep=" ",dtype=np.int)
+            cellAtomsCopy = np.copy(cellAtoms)
             cellSize = np.sum(cellAtoms)
         elif i == 6 + offset:
             cellInputType = line[0]
@@ -109,32 +112,52 @@ for d,p in zip(directions,
     multiplyers.append(1+int(cutOff/d[p]))
 
 periodicTable = {0  :"H", 1  :"He", 2  :"Li", 3  :"Be", 4  :"B", 5  :"C", 6  :"N", 7  :"O", 8  :"F", 9 :"Ne", 10 :"Na", 11 :"Mg", 12 :"Al", 13 :"Si", 14 :"P", 15 :"S", 16 :"Cl", 17 :"Ar", 18 :"K", 19 :"Ca", 20 :"Sc", 21 :"Ti", 22 :"V", 23 :"Cr", 24 :"Mn", 25 :"Fe", 26 :"Co", 27 :"Ni", 28 :"Cu", 29 :"Zn", 30 :"Ga", 31 :"Ge", 32 :"As", 33 :"Se", 34 :"Br", 35 :"Kr", 36 :"Rb", 37 :"Sr", 38 :"Y", 39 :"Zr", 40 :"Nb", 41 :"Mo", 42 :"Tc", 43 :"Ru", 44 :"Rh", 45 :"Pd", 46 :"Ag", 47 :"Cd", 48 :"In", 49 :"Sn", 50 :"Sb", 51 :"Te", 52 :"I", 53 :"Xe", 54 :"Cs", 55 :"Ba", 56 :"La", 57 :"Ce", 58 :"Pr", 59 :"Nd", 60 :"Pm", 61 :"Sm", 62 :"Eu", 63 :"Gd", 64 :"Tb", 65 :"Dy", 66 :"Ho", 67 :"Er", 68 :"Tm", 69 :"Yb", 70 :"Lu", 71 :"Hf", 72 :"Ta", 73 :"W", 74 :"Re", 75 :"Os", 76 :"Ir", 77 :"Pt", 78 :"Au", 79 :"Hg", 80 :"Tl", 81 :"Pb", 82 :"Bi", 83 :"Po", 84 :"At", 85 :"Rn", 86 :"Fr", 87 :"Ra", 88 :"Ac", 89 :"Th", 90 :"Pa", 91 :"U", 92 :"Np", 93 :"Pu", 94 :"Am", 95 :"Cm", 96 :"Bk", 97 :"Cf", 98 :"Es", 99:"Fm", 100:"Md", 101:"No", 102:"Lr", 103:"Rf", 104:"Db", 105:"Sg", 106:"Bh", 107:"Hs", 108:"Mt", 109:"Ds", 110:"Rg", 111:"Cn", 112:"Nh", 113:"Fl", 114:"Mc", 115:"Lv", 116:"Ts", 117:"Og"}
-crystal = []
 
+crystal = []
 numberOfAtoms = 0
 output = ""
-for x in range(-multiplyers[0],multiplyers[0]+1):
-    for y in range(-multiplyers[1],multiplyers[1]+1):
-        for z in range(-multiplyers[2],multiplyers[2]+1):
+for x in range(multiplyers[0]+1):
+    for y in range(multiplyers[1]+1):
+        for z in range(multiplyers[2]+1):
             for atom in cell:
                 position = np.copy(atom[1])
                 for a,n in zip([x,y,z],directions):
                     position += a*n
-                if np.linalg.norm(position - cellCenter) < cutOff :    
-                    numberOfAtoms += 1
-                    if atom[0] < len(atomNames):
-                        output += "%s"%atomNames[int(atom[0])]
-                    else:    
-                        output += "%s"%periodicTable[int(atom[0])]
-                    for p in position:
-                        output += " %.10f"%p
-                    output += "\n"
+                numberOfAtoms += 1
+                if atom[0] < len(atomNames):
+                    flag = "%s"%atomNames[int(atom[0])]
+                else:    
+                    flag = "%s"%periodicTable[int(atom[0])]
+                crystal.append([flag,position])    
 
-print(numberOfAtoms)
-print("")
-print(output)
+with open("output/crystal.xyz","w+") as xyzFile:
+    xyzFile.write(str(numberOfAtoms))
+    xyzFile.write("\n\n")
+    for atom in crystal:
+        xyzFile.write("%s"%atom[0])
+        for xyz in atom[1]:
+            xyzFile.write(" %.10f"%xyz)
+        xyzFile.write("\n")
+    xyzFile.write("\n")
 
-exit()
-print(comment)
-print(directions)
-print(cell)
+with open("output/POSCAR","w+") as vaspFile:
+    vaspFile.write(comment)
+    vaspFile.write("\n1.0\n")
+    for m,d in zip(multiplyers,directions):
+        for field in d: 
+            vaspFile.write("  %.10f"%(m*field))
+        vaspFile.write("\n")
+    for atomName in atomNames:
+        vaspFile.write("%s "%atomName)
+    vaspFile.write("\n")    
+    for atomNumber in cellAtomsCopy:
+        vaspFile.write("%d "%(np.prod(np.array(multiplyers)+1)*atomNumber))
+    vaspFile.write("\nCarthesian\n")
+    for atomName in atomNames:
+        for atom in crystal:
+            if atom[0]==atomName:
+                for vasp in atom[1]:
+                    vaspFile.write(" %.10f "%vasp)
+                vaspFile.write(" %s\n"%atom[0])
+    vaspFile.write("\n")
+
