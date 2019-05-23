028d91d5a602daf00b214bd688489d4261cdb96d
Mellechowicz <apkadzielawa@gmail.com>
2019-05-23 16:49:20 +0200

    Making my code pretty at czw, 23 maj 2019, 16:49:20 CEST

--- JorG/equivalent.py
+++ JorG/equivalent.py
@@ -9,14 +9,14 @@ def find_unique_flips(referenceAtom,crystal,symmetry,cutOff,
                       atomTypeMask=maskFull,
                       Wyckoffs="abcdefghijklmnopqrstuvwxyz",
                       wyckoffDict={'a':'a','b':'b','c':'c','d':'d','e':'e','f':'f','g':'g','h':'h','i':'i','j':'j','k':'k','l':'l','m':'m','n':'n','o':'o','p':'p','q':'q','r':'r','s':'s','t':'t','u':'u','v':'v','w':'w','x':'x','y':'y','z':'z'}, 
-                      logAccuracy=2, VERBOSE=False):
+                      logAccuracy=2):
 
     distances = []
     for i,atom in enumerate(crystal):
        d = np.around(np.linalg.norm(atom[1]-referenceAtom[1]), decimals=logAccuracy)
        if ((d,atom[0]) not in distances and
             wyckoffDict[symmetry['wyckoffs'][i]] in Wyckoffs and
-            d > 0.0 and d <= cutOff ):
+            d > 0.0 and d <= cutOff and '$'+atom[0]+'$' in atomTypeMask):
             distances.append((d,atom[0]))
     
     distances = np.array(distances, dtype=[('distance', np.float), ('element', 'U3')])
@@ -26,18 +26,17 @@ def find_unique_flips(referenceAtom,crystal,symmetry,cutOff,
     flipper = [] 
     case = 1
     for (d,n) in distances:
-        if "$"+n+"$" in atomTypeMask:
-            for i,atom in enumerate(crystal):
-                if not checker[i]:
-                    if d == np.around(np.linalg.norm(atom[1]-referenceAtom[1]),decimals=logAccuracy) and atom[0] == n:
-                        for j in np.argwhere(symmetry['equivalent_atoms'] == symmetry['equivalent_atoms'][i]).flatten():
-                            if d == np.around(np.linalg.norm(crystal[j][1]-referenceAtom[1]),decimals=logAccuracy) and wyckoffDict[symmetry['wyckoffs'][j]] in Wyckoffs:
-                                if(VERBOSE):
-                                    print("Case no: %d, for %s with (wyck: %s; ref.wyck: %s) at d= %f"%(case,crystal[j][0],symmetry['wyckoffs'][j],wyckoffDict[symmetry['wyckoffs'][j]],d))
-                                checker[j] = True
-                        if checker[i]:
-                            flipper.append((i,atom,d,wyckoffDict[symmetry['wyckoffs'][i]]))
-                            case += 1
+        for i,atom in enumerate(crystal):
+            if checker[i]:
+                continue
+            if d == np.around(np.linalg.norm(atom[1]-referenceAtom[1]),decimals=logAccuracy)\
+              and atom[0] == n:
+                for j in np.argwhere(symmetry['equivalent_atoms'] == symmetry['equivalent_atoms'][i]).flatten():
+                    if d == np.around(np.linalg.norm(crystal[j][1]-referenceAtom[1]),decimals=logAccuracy)\
+                      and wyckoffDict[symmetry['wyckoffs'][j]] in Wyckoffs:
+                        checker[j] = True
+                flipper.append((i,atom,d,wyckoffDict[symmetry['wyckoffs'][i]]))
+                case += 1
     flipper = np.array(flipper, dtype=[('id', int), ('atom', list), ('distance', np.float), ('wyckoff', 'U1')])
     flipper.sort(order='distance')
     return flipper
@@ -52,7 +51,7 @@ def find_all_flips(referenceAtom,crystal,symmetry,cutOff,
                    atomTypeMask=maskFull,
                    Wyckoffs="abcdefghijklmnopqrstuvwxyz",
                    wyckoffDict={'a':'a','b':'b','c':'c','d':'d','e':'e','f':'f','g':'g','h':'h','i':'i','j':'j','k':'k','l':'l','m':'m','n':'n','o':'o','p':'p','q':'q','r':'r','s':'s','t':'t','u':'u','v':'v','w':'w','x':'x','y':'y','z':'z'}, 
-                   logAccuracy=2, VERBOSE=False):
+                   logAccuracy=2):
    
     flipper = []
     for i,atom in enumerate(crystal): 
--- aux/symmetry.py
+++ aux/symmetry.py
@@ -24,11 +24,55 @@ from .periodic import periodicTableElement
 from .format import *
 from sys import stdout
 import numpy as np
+def write_single(comment, record, crystal,
+                 raport, atomDict, linewidth=88):
+    uniqueWyckoffs = set(record['wyckoffs'])
+    wyckoffCount =  dict.fromkeys(uniqueWyckoffs,0)
+    raport.write(color.INV+3*"*"+(comment).center(linewidth-6)+3*"*"+color.END+"\n")
+    raport.write(linewidth*"*"+"\n")
+    
+    offset = len(color.BF+color.DARKMAGENTA+color.DARKGREEN+color.END)
+    raport.write(3*"*"+("Spacegroup: "+color.BF+color.DARKMAGENTA
+                      +"%s "%(record['international'])
+                      +color.DARKGREEN+"(%d) "%(record['number'])
+                      +color.END).center(linewidth-6+offset)
+                      +3*"*"+'\n')
+    raport.write(3*"*"+("Mapping to equivalent atoms with the Wyckoff positions:".center(linewidth-6)+3*"*"+'\n'))
+
+    offset=len(color.BF+color.DARKGREEN+color.GRAY+color.DARKGRAY+color.GRAY+color.END+color.DARKBLUE+color.END) 
+   
+    for i, (x,atom,wyck) in enumerate(zip(record['equivalent_atoms'],crystal,record['wyckoffs'])):
+        output = color.BF+color.DARKGREEN
+        try:
+            output += "%s: "%(atomDict[atom[0]])
+        except:
+            output += "%s: "%(atom[0])
+        output += color.GRAY+" %d "%(i+1)+color.DARKGRAY+" -> "
+        output += color.GRAY+" %d "%(x+1)+color.END+" W: "
+        output += color.DARKBLUE+"%s"%(wyck)+color.END
+        output = 3*"*"+(output.center(linewidth-6+offset)+3*"*"+"\n")
+        raport.write(output)
+        wyckoffCount[wyck] += 1 
+
+    raport.write(linewidth*"*"+"\n")
+
+    part=len(color.BF+color.DARKRED+color.END+color.BF+color.DARKGREEN+color.END)
+    offset=0
+    output = ""
+    for wyck in uniqueWyckoffs:
+        output += color.BF+color.DARKRED+" #%s "%(wyck)+color.END
+        output += " = "+color.BF+color.DARKGREEN+" %d "%(wyckoffCount[wyck])+color.END
+        offset += part
+    raport.write(3*"*"+(output.center(linewidth-6+offset)+3*"*"+"\n"))
+
+    raport.write(linewidth*"*"+"\n")
+ 
 def write_report(comments,data,crystal,fileName=None, atomDict=None, linewidth=88):
-    if fileName is None:
-        raport = stdout
-    else:
+    try:
         raport = open(fileName,"w+")
+    except:
+        raport = stdout
+
    
     offset=len(color.BF+color.DARKYELLOW+color.INV+color.END)
     raport.write(linewidth*"*"+"\n")
@@ -36,45 +80,7 @@ def write_report(comments,data,crystal,fileName=None, atomDict=None, linewidth=8
     raport.write(linewidth*"*"+"\n")
 
     for i,(comment,record) in enumerate(zip(comments,data)):
-        uniqueWyckoffs = set(record['wyckoffs'])
-        wyckoffCount =  dict.fromkeys(uniqueWyckoffs,0)
-        raport.write(color.INV+3*"*"+(comment).center(linewidth-6)+3*"*"+color.END+"\n")
-        raport.write(linewidth*"*"+"\n")
-        
-        raport.write(3*"*"+("Spacegroup: "+color.BF+color.DARKMAGENTA
-                          +"%s "%(record['international'])
-                          +color.DARKGREEN+"(%d) "%(record['number'])
-                          +color.END).center(linewidth-6+len(color.BF+color.DARKMAGENTA+color.DARKGREEN+color.END))
-                          +3*"*"+'\n')
-        raport.write(3*"*"+("Mapping to equivalent atoms with the Wyckoff positions:".center(linewidth-6)+3*"*"+'\n'))
-
-        offset=len(color.BF+color.DARKGREEN+color.GRAY+color.DARKGRAY+color.GRAY+color.END+color.DARKBLUE+color.END) 
-        if atomDict is None:
-            for i, (x,atom,wyck) in enumerate(zip(record['equivalent_atoms'],crystal,record['wyckoffs'])):
-                raport.write(3*"*"+(color.BF+color.DARKGREEN+"%s: "%(atom[0])
-                                   +color.GRAY+" %d "%(i+1)+color.DARKGRAY+" -> "
-                                   +color.GRAY+" %d "%(x+1)+color.END+" W: "
-                                   +color.DARKBLUE+"%s"%(wyck)+color.END).center(linewidth-6+offset)+3*"*"+"\n")
-                wyckoffCount[wyck] += 1 
-        else: 
-            for i, (x,atom,wyck) in enumerate(zip(record['equivalent_atoms'],crystal,record['wyckoffs'])):
-                raport.write(3*"*"+(color.BF+color.DARKGREEN+"%s: "%(atomDict[atom[0]])
-                                   +color.GRAY+" %d "%(i+1)+color.DARKGRAY+" -> "
-                                   +color.GRAY+" %d "%(x+1)+color.END+" W: "
-                                   +color.DARKBLUE+"%s"%(wyck)+color.END).center(linewidth-6+offset)+3*"*"+"\n")
-                wyckoffCount[wyck] += 1 
-
-        raport.write(linewidth*"*"+"\n")
-
-        part=len(color.BF+color.DARKRED+color.END+color.BF+color.DARKGREEN+color.END)
-        offset=0
-        output = ""
-        for wyck in uniqueWyckoffs:
-            output += color.BF+color.DARKRED+" #%s "%(wyck)+color.END+" = "+color.BF+color.DARKGREEN+" %d "%(wyckoffCount[wyck])+color.END
-            offset+=part
-        raport.write(3*"*"+(output.center(linewidth-6+offset)+3*"*"+"\n"))
-
-        raport.write(linewidth*"*"+"\n")
+        write_single(comment,record,crystal,raport,atomDict,linewidth)
     if raport is not stdout:
         raport.close()
     
