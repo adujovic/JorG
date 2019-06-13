920538dcfaacbb861a01f5dfd440b8a69769bb09
Andrzej P. Kądzielawa <apkadzielawa@gmail.com>
2019-06-13 14:25:48 +0200

    Update run_all_tests.sh

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -4,6 +4,11 @@ echo "starting tests:"
 echo "python3 -m unittest tests.POSCARloader._unittest -v"
 python3 -m unittest tests.POSCARloader._unittest -v
 echo "                 ...done"
+
 echo "python3 -m unittest tests.NearestNeighborsGenerator._unittest -v"
 python3 -m unittest tests.NearestNeighborsGenerator._unittest -v
+echo "                 ...done"
+
+echo "python3 -m unittest tests.argv._unittest -v"
+python3 -m unittest tests.argv._unittest -v
 echo "                 ...done"
