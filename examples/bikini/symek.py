a3e97002775bb0d4fda2fae3452f58bdb2794f93
Andrzej Kądzielawa <apkadzielawa@gmail.com>
2019-06-17 12:40:22 +0200

    Modyfing main files for clarity as well as deployment

--- examples/bikini/symek.py
+++ examples/bikini/symek.py
@@ -27,153 +27,201 @@ from WiP.WiP import StreamHandler,JmolVisualization
 from WiP.WiP import TemporaryFiles,errors,Msg
 from WiP.WiP import VariableFixer,Symmetry
 
-def main(**args):
-    pass
+class JorGpi:
+    def __init__(self,*args):
+        self.currentOptions  = options(*args)
+        self.cutOff          = self.currentOptions('cutOff')
+        self.nearestNeighbor = self.currentOptions('neighbor')
+        self.reference       = self.currentOptions('reference')
+        self.outDirName      = self.currentOptions('output')
+        self.extraMultiplier = np.zeros(3,dtype=int)
+    
+        self.handler    = StreamHandler(self.outDirName)
+        self.outDirName = self.handler()
+    
+    #   Reading POSCAR and INCAR files
+        self.readData,self.oldMoments,self.incarData =\
+                self.handler.load_VASP(self.currentOptions('input'),
+                                       self.currentOptions('incar'))
+        self.cell          = self.readData['cell']
+        self.atomNames     = self.readData['atomNames']
+        self.comment       = self.readData['comment']
+        self.directions    = self.readData['directions']
+        self.referenceAtom,self.reference =\
+                VariableFixer.fix_reference(self.reference,self.cell,
+                                            self.currentOptions('mask'))
+        self.symmetry      = Symmetry(self.readData['cellSymmetry'])
+        self.logAccuracy   = 2  #  accuracy of distance is 10^(-logAccuracy)
 
-if __name__ == '__main__':
-    currentOptions = options(*argv)
-    cutOff         = currentOptions('cutOff')
-    nearestNeighbor= currentOptions('neighbor')
-    reference      = currentOptions('reference')
-    outDirName     = currentOptions('output')
-    extraMultiplier = np.zeros(3,dtype=int)
+    #   Setting options
+        if self.currentOptions('symmetry'):
+            self.symmetry_run()
+            exit(0)
+        if self.currentOptions('refined'):
+            refinedCell               = self.symmetry.standarize()
+            self.cell,self.directions = VariableFixer.from_refined(refinedCell) 
 
-    handler    = StreamHandler(outDirName)
-    outDirName = handler()
-
-#    """ Reading POSCAR and INCAR files
-#                                       """
-    readData,oldMoments,incarData =\
-            handler.load_VASP(currentOptions('input'),currentOptions('incar'))
-    cell          = readData['cell']
-    atomNames     = readData['atomNames']
-    comment       = readData['comment']
-    directions    = readData['directions']
-    referenceAtom,reference = VariableFixer.fix_reference(reference,cell,
-                                                          currentOptions('mask'))
-#    """ Checking the symmetry
-#                    of the input """
-    symmetry      = Symmetry(readData['cellSymmetry'])
-    if(currentOptions('symmetry')):
-        symmetryStandard,symmetryRefined = symmetry.get_standarized()
+    def symmetry_run(self):
+#   Checking the symmetry of the input
+        symmetryStandard,symmetryRefined = self.symmetry.get_standarized()
         JorG.symmetry.write_report(["(1) the crude input cell",
-                      "(2) the standarized cell",
-                      "(3) the refined cell"],
-                [symmetry.symmetry,symmetryStandard,symmetryRefined],cell)
-        exit(0)
-    else:
-        with open(outDirName+"/input_report.txt",'w+') as raport:
-            JorG.symmetry.write_report(["Analysis of symmetry in the input cell"], [symmetry.symmetry], cell,stream=raport)
-                     
-    if currentOptions('refined'):
-        refinedCell = symmetry.standarize()
-        cell,directions = VariableFixer.from_refined(refinedCell) 
+                                    "(2) the standarized cell",
+                                    "(3) the refined cell"],
+                [self.symmetry.symmetry,symmetryStandard,symmetryRefined],self.cell)
 
-#    """
-#        Printing input data
-#    """
-    Msg.print_crystal_info(title="INPUT",crystal=cell,directions=directions,
-                           reference=reference,moments=oldMoments)
+    def write_input_raport(self):
+        with open(self.outDirName+"/input_report.txt",'w+') as raport:
+            JorG.symmetry.write_report(["Analysis of symmetry in the input cell"],
+                                       [self.symmetry.symmetry], self.cell, stream=raport)
+        Msg.print_crystal_info(title="INPUT",crystal=self.cell,directions=self.directions,
+                               reference=self.reference,moments=self.oldMoments)
+                 
+    def initialize_new_cell(self):
+        self.nearestNeighbor,\
+                self.cutOff = VariableFixer.fix_neighbor(self.nearestNeighbor,self.cutOff)
+        if self.cutOff is None:
+            self.prepare_cell_from_NN()
+        else:
+            self.prepare_cell_from_RR()
+        self.search_for_flipps()
+        self.write_output_raport()
 
-    nearestNeighbor,cutOff = VariableFixer.fix_neighbor(nearestNeighbor,cutOff)
-
-    if cutOff is None:
-        generatorNN = generator.NearestNeighborsGenerator(cell,referenceAtom,directions)
-        generatorNN.wyckoffs         = currentOptions('Wyckoffs')
-        generatorNN.atomTypeMask     = currentOptions('mask')
-        generatorNN.moments          = oldMoments
-        generatorNN.extraMultiplier  = extraMultiplier
+    def prepare_cell_from_NN(self):
+        generatorNN = generator.NearestNeighborsGenerator(self.cell,self.referenceAtom,self.directions)
+        generatorNN.wyckoffs         = self.currentOptions('Wyckoffs')
+        generatorNN.atomTypeMask     = self.currentOptions('mask')
+        generatorNN.moments          = self.oldMoments
+        generatorNN.extraMultiplier  = self.extraMultiplier
 
         try:
-            (cutOff, crystal, symmetryFull, newReference,
-                      copiesInEachDirection, wyckoffDict) = generatorNN(nearestNeighbor)
+            (self.cutOff, self.crystal,
+             self.symmetryFull, self.newReference,
+             self.copiesInEachDirection, self.wyckoffDict) = generatorNN(self.nearestNeighbor)
         except Exception:
             print("Failed to generate crystal")
             exit(errors.failed_to_generate)
-        extraDirections = VariableFixer.fix_directions(copiesInEachDirection,directions)
-    else:
-        copiesInEachDirection = generator.get_number_of_pictures(directions,cutOff,referenceAtom)
-        localGenerator = generator.CrystalGenerator(cell,directions, atomNames,reference=reference)
-        localGenerator.moments = oldMoments
-        crystal, newReference = localGenerator(copiesInEachDirection)
-        extraDirections = VariableFixer.fix_directions(copiesInEachDirection,directions)
-        wyckoffDict, symmetryFull, symmetryOriginal =\
-           generator.wyckoffs_dict(generator.NearestNeighborsGenerator.get_symmetry(cell,
-                                                                             directions), 
-                                   generator.NearestNeighborsGenerator.get_symmetry(crystal,
-                                                                           extraDirections))
+        self.extraDirections =\
+                VariableFixer.fix_directions(self.copiesInEachDirection,self.directions)
 
-#    """ Checking the symmetry
-#                    of the output """
-    with open(outDirName+"/output.txt",'w+') as raport:
-        JorG.symmetry.write_report(["Analysis of symmetry in the generated cell"],
-                     [symmetryFull], crystal, stream=raport)
-    loadsave.save_POSCAR(readData, fileName=outDirName+"/POSCAR",
-                         crystal=crystal, multiplyers=copiesInEachDirection)
+    def prepare_cell_from_RR(self):
+        self.copiesInEachDirection =\
+                generator.get_number_of_pictures(self.directions,
+                                                 self.cutOff,
+                                                 self.referenceAtom)
+        localGenerator =\
+                generator.CrystalGenerator(self.cell, self.directions,
+                                           self.atomNames,reference=self.reference)
+        localGenerator.moments          = self.oldMoments
+        self.crystal, self.newReference = \
+                localGenerator(self.copiesInEachDirection)
+        self.extraDirections            = \
+                VariableFixer.fix_directions(self.copiesInEachDirection,self.directions)
+        self.wyckoffDict, self.symmetryFull, self.symmetryOriginal =\
+           generator.wyckoffs_dict(
+                   generator.NearestNeighborsGenerator.get_symmetry(self.cell,
+                                                                    self.directions), 
+                   generator.NearestNeighborsGenerator.get_symmetry(self.crystal,
+                                                                    self.extraDirections))
 
-#   """
+    def search_for_flipps(self):
 #        Searching for unique atoms for calculations
-#                                                    """
-    logAccuracy  = 2     #  accuracy of distance is 10^(-logAccuracy)
+        flipSearch              = findFlips()
+        flipSearch.symmetry     = self.symmetryFull
+        flipSearch.crystal      = self.crystal
+        flipSearch.atomTypeMask = self.currentOptions('mask')
+        flipSearch.Wyckoffs     = self.currentOptions('Wyckoffs')
+        flipSearch.wyckoffDict  = self.wyckoffDict
+        flipSearch.logAccuracy  = self.logAccuracy
+        self.flipper            = flipSearch.unique(self.crystal[self.newReference],self.cutOff)
+        self.allFlippable       = flipSearch.all(self.crystal[self.newReference],self.cutOff)
 
-    flipSearch              = findFlips()
-    flipSearch.symmetry     = symmetryFull
-    flipSearch.crystal      = crystal
-    flipSearch.atomTypeMask = currentOptions('mask')
-    flipSearch.Wyckoffs     = currentOptions('Wyckoffs')
-    flipSearch.wyckoffDict  = wyckoffDict
-    flipSearch.logAccuracy  = logAccuracy
-    flipper                 = flipSearch.unique(crystal[newReference],cutOff)
-    allFlippable            = flipSearch.all(crystal[newReference],cutOff)
+        Msg.print_crystal_info(title="OUTPUT",crystal=self.crystal,directions=self.extraDirections,
+                               copies=(*VariableFixer.add_to_all(self.copiesInEachDirection),),
+                               reference=self.newReference)
 
-    Msg.print_crystal_info(title="OUTPUT",crystal=crystal,directions=extraDirections,
-                           copies=(*VariableFixer.add_to_all(copiesInEachDirection),),
-                           reference=newReference)
-
-    selected = [newReference]
-    for caseID,(i,atom,distance,wyck) in enumerate(flipper):
-        print_case(atom,atomID=i+1,caseID=caseID+1,wyckoffPosition=wyck,distance=distance)
-        selected.append(i)
-    crystal8 = generator.apply_mirrorsXYZ(extraDirections,crystal,
-                                cutOff=cutOff, reference=newReference)
-    loadsave.save_xyz(crystal, fileName=outDirName+"/crystal.xyz",    selectedAtoms = selected)
-    loadsave.save_xyz(crystal8,fileName=outDirName+"/crystalFull.xyz",selectedAtoms = selected)
-    JmolVisualization.create_script(outDirName,radius=cutOff,center=crystal[newReference][1])
+    def write_output_raport(self):
+    #   Checking the symmetry of the output
+        with open(self.outDirName+"/output.txt",'w+') as raport:
+            JorG.symmetry.write_report(["Analysis of symmetry in the generated cell"],
+                         [self.symmetryFull], self.crystal, stream=raport)
+        loadsave.save_POSCAR(self.readData, fileName=self.outDirName+"/POSCAR",
+                             crystal=self.crystal, multiplyers=self.copiesInEachDirection)
     
-    tmpFiles = TemporaryFiles()
-    tmpFiles.write_input(allFlippable,crystal)
-    tmpFiles.write_supercell(crystal)
-    tmpFiles.write_directions(extraDirections)
+        self.selected = [self.newReference]
+        for caseID,(i,atom,distance,wyck) in enumerate(self.flipper):
+            print_case(atom,atomID=i+1,caseID=caseID+1,wyckoffPosition=wyck,distance=distance)
+            self.selected.append(i)
+        self.crystal8 =\
+                generator.apply_mirrorsXYZ(self.extraDirections,self.crystal,
+                                           cutOff=self.cutOff, reference=self.newReference)
+        loadsave.save_xyz(self.crystal, fileName=self.outDirName+"/crystal.xyz",
+                          selectedAtoms = self.selected)
+        loadsave.save_xyz(self.crystal8,fileName=self.outDirName+"/crystalFull.xyz",
+                          selectedAtoms = self.selected)
+        JmolVisualization.create_script(self.outDirName,radius=self.cutOff,
+                                        center=self.crystal[self.newReference][1])
 
-    Msg.print_solver_status(int(2**len(allFlippable)),tmpFiles)
-    call('cd asa/solver; make clean; make SITES=-D_SITESNUMBER=%d; cd ../../'%len(crystal), shell=True)
-    call('./asa/solver/start %s %d %d'%(str(tmpFiles),newReference,4*nearestNeighbor+8), shell=True)
-    call('cd ./asa/solver; make clean; cd ../../', shell=True)
-    del tmpFiles
+    class AdaptiveSimulatedAnnealing:
+        solverDirectory = './asa/solver'
+        myDirectory     = '../../'
+        def __init__(self,JorGpiObject):
+            self.tmpFiles = TemporaryFiles()
+            self.tmpFiles.write_input(JorGpiObject.allFlippable,JorGpiObject.crystal)
+            self.tmpFiles.write_supercell(JorGpiObject.crystal)
+            self.tmpFiles.write_directions(JorGpiObject.extraDirections)
+            Msg.print_solver_status(int(2**len(JorGpiObject.allFlippable)),self.tmpFiles)
+            call('cd %s; make clean; make SITES=-D_SITESNUMBER=%d; cd %s'%(self.solverDirectory,len(JorGpiObject.crystal),self.myDirectory), shell=True)
 
-    flippingConfigurations = np.loadtxt('best.flips',bool)
-    try:
-        np.shape(flippingConfigurations)[1]
-    except IndexError:
-        flippingConfigurations=[flippingConfigurations]
+        def __call__(self,JorGpiObject):
+            call('%s/start %s %d %d'%(self.solverDirectory,str(self.tmpFiles),JorGpiObject.newReference,4*JorGpiObject.nearestNeighbor+8), shell=True)
+            call('cd %s; make clean; cd %s'%(self.solverDirectory,self.myDirectory), shell=True)
 
-    gen = NaiveHeisenberg(flippingConfigurations,crystal,crystal8)
-    systemOfEquations = gen.generate(currentOptions('mask'),[flip[2] for flip in flipper])
+        def __del__(self):
+            Msg.print_solver_status(len(np.loadtxt('best.flips',bool)),self.tmpFiles)
+            del self.tmpFiles
 
-    eqs = EquationSolver(systemOfEquations,np.zeros(len(systemOfEquations)))
-    systemOfEquations = eqs.remove_tautologies()
-    remover = eqs.remove_linears()
-    if remover:
-        flippingConfigurations = np.delete(flippingConfigurations, tuple(remover), axis=0)
-        systemOfEquations = eqs.equations
-    if systemOfEquations.size == 0:
-        print("ERROR! Not enough equations! Please rerun.")
-        exit(-3)
-    if not currentOptions('redundant'): # If the System of Equations is required to be consistent
-        systemOfEquations,flippingConfigurations = eqs.remove_linear_combinations(flippingConfigurations)
+    def load_from_annealing(self):
+        self.flippingConfigurations = np.loadtxt('best.flips',bool)
+        try:
+            np.shape(self.flippingConfigurations)[1]
+        except IndexError:
+            self.flippingConfigurations=[self.flippingConfigurations]
 
-    Msg.print_equations(systemOfEquations,currentOptions('redundant'))
-    np.savetxt(outDirName+'/systemOfEquations.txt',systemOfEquations)
-    saver = loadsave.INCARsaver(incarData,crystal)
-    saver.save(outDirName,flippingConfigurations)
+    def build_system_of_equations(self,flippingConfigurations):
+        gen = NaiveHeisenberg(flippingConfigurations,self.crystal,self.crystal8)
+        systemOfEquations = gen.generate(self.currentOptions('mask'),[flip[2] for flip in self.flipper])
+
+        eqs = EquationSolver(systemOfEquations,np.zeros(len(systemOfEquations)))
+        systemOfEquations = eqs.remove_tautologies()
+        remover = eqs.remove_linears()
+        if remover:
+            flippingConfigurations = np.delete(flippingConfigurations, tuple(remover), axis=0)
+            systemOfEquations = eqs.equations
+        if systemOfEquations.size == 0:
+            print("ERROR! Not enough equations! Please rerun.")
+            exit(-3)
+        if not self.currentOptions('redundant'): # If the System of Equations is required to be consistent
+            systemOfEquations,flippingConfigurations = eqs.remove_linear_combinations(flippingConfigurations)
+        return systemOfEquations,flippingConfigurations
+
+    def save_result(self):
+        Msg.print_equations(self.systemOfEquations,self.currentOptions('redundant'))
+        np.savetxt(self.outDirName+'/systemOfEquations.txt',self.systemOfEquations)
+        saver = loadsave.INCARsaver(self.incarData,self.crystal)
+        saver.save(self.outDirName,self.flippingConfigurations)
+
+    def generate_possible_configurations(self):
+        solver = self.AdaptiveSimulatedAnnealing(self)
+        solver(self)
+        del solver
+        self.load_from_annealing()
+        self.systemOfEquations,self.flippingConfigurations = \
+                self.build_system_of_equations(self.flippingConfigurations)
+        self.save_result()
+
+
+if __name__ == '__main__':
+    engine = JorGpi(*argv)
+    engine.initialize_new_cell()
+    engine.generate_possible_configurations()
     exit()
