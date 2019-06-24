e91a19e07e2f8811bbd5453007e69bfbdee82e63
Andrzej Kądzielawa <apkadzielawa@gmail.com>
2019-06-24 13:59:01 +0200

    test 02

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
+ERR=0
 
 echo "starting tests:"
 echo "python3 -m unittest tests.POSCARloader._unittest -v"
 python3 -m unittest tests.POSCARloader._unittest -v
+TST=$?
+if [ "$TST" -ne "0" ]; then
+    ERR=$TST
+fi
 echo "                 ...done"
 
 echo "python3 -m unittest tests.NearestNeighborsGenerator._unittest -v"
 python3 -m unittest tests.NearestNeighborsGenerator._unittest -v
+TST=$?
+if [ "$TST" -ne "0" ]; then
+    ERR=$TST
+fi
 echo "                 ...done"
 
 echo "python3 -m unittest tests.argv._unittest -v"
 python3 -m unittest tests.argv._unittest -v
+TST=$?
+if [ "$TST" -ne "0" ]; then
+    ERR=$TST
+fi
 echo "                 ...done"
 
-exit -1
+exit $ERR
