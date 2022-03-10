9e38a6a1e30fa8d70fe1409ddbdaf29c632b6b9e
adujovic <86768942+adujovic@users.noreply.github.com>
2022-03-10 13:51:15 +0100

    Class for building & running the serial Ising...
    ...model solver. Added inheritance from the abstract class CrunSetup.

--- JorGpi/generate/crun.py
+++ JorGpi/generate/crun.py
@@ -1,40 +1,16 @@
 # -*- coding: utf-8 -*-
 
-from os import scandir,mkdir
+from os import scandir
 import re
 from ctypes import CDLL,c_char_p
-import importlib
-import setuptools
+from JorGpi.generate.crunsetup import CrunSetup
 
-class Crun:
-    compilerOptions= { 'include_dirs'       : ['/usr/local/include'],
-                       'extra_compile_args' : ['-O3','-Wall','-pedantic']}
-
+class Crun(CrunSetup):
     def __init__(self,*args,**kwargs):
-        importlib.reload(setuptools)
-        try:
-            mkdir('build')
-        except Exception as ext:
-            print(ext,": ignoring")
-        self.files = args
-        self.compilerOptions.update(kwargs)
-
-        if 'name' in kwargs:
-            self.name = kwargs['name']
-            del self.compilerOptions['name']
-        else:
-            self.name = re.sub('^.*/','',args[0])
-            self.name = re.sub('\..*$','',self.name)
-
-        self.module = setuptools.Extension(self.name, sources = list(args), **self.compilerOptions)
-
-        setuptools.setup(
-                script_args=['build','--build-lib=%s-lib'%(self.name)],
-                ext_modules=[self.module])
-
-        setuptools.setup(name=self.name,script_args=['clean'],
-               ext_modules = [self.module])
-
+        super().__init__(*args,**kwargs)
+        super().build(*args)
+        super().clean_module()
+        
         self.search_for_library()
 
     def search_for_library(self):
@@ -60,10 +36,6 @@ class Crun:
                 argStr.append(c_char_p(arg.encode('utf-8')))
             else:
                 argStr.append(arg)
+    
         f = eval("self.library.%s"%function)
         return f(*argStr)
-
-    def __del__(self):
-        setuptools.setup(script_args=['clean','--all','--build-lib=%s-lib'%(self.name)],
-               ext_modules = [self.module])
-        importlib.reload(setuptools)
